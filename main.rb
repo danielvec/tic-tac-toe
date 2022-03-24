da96648c373680c4bb2d7dd47b65d1ae250f8ff9
DanielVec <vechioladaniel@gmail.com>
2022-03-23 20:52:45 -0700

    put display board method into module to use with player and game class. add methods to player class to make move and reflect this in game class

--- main.rb
+++ main.rb
@@ -1,23 +1,61 @@
+module DisplayBoard
+  def display_board
+    BOARD.each do |row|
+      p row
+    end
+  end
+end
+
 class Game
-  BOARD = [%w[_ _ _], %w[_ _ _], %w[_ _ _]]
+  include DisplayBoard
   def initialize(player_one, player_two)
     @player_one = player_one
     @player_two = player_two
     display_board
-    puts @player_one.mark
-  end
-
-  def display_board
-    BOARD.each do |row|
-        p row
-      end
+    if @player_one.mark == 'x'
+      @player_one.make_move
+    else
+      @player_two.make_move
+    end
   end
 end
 
 class Player
+  include DisplayBoard
   attr_reader :mark
   def initialize(mark)
     @mark = mark
+  end
+
+  def make_move
+    puts "#{self.mark}'s turn"
+    get_row
+    get_column
+    if BOARD[@row][@column] == "_"
+      BOARD[@row][@column] = self.mark
+      display_board
+    else
+      puts 'spot already taken'
+      make_move
+    end
+  end
+
+  def get_row
+    puts 'choose a row between 1 and 3'
+    @row = gets.to_i - 1
+    unless @row > -1 && @row < 3
+      puts 'not a valid row'
+      get_row
+    end
+  end
+
+  def get_column
+    puts 'choose a column between 1 and 3'
+    @column = gets.to_i - 1
+    unless @column > -1 && @column < 3
+      puts 'not a valid column'
+      get_column
+    end
   end
 end
 
@@ -36,5 +74,5 @@ class Choice
     Game.new(player_one, player_two)
   end
 end
-
+BOARD = [%w[_ _ _], %w[_ _ _], %w[_ _ _]]
 Choice.new
